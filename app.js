55246fefbbb8bf3a9a9412bd07f563dc2162d4ed
Mdltre <68366944+Mdltre@users.noreply.github.com>
2022-09-23 13:51:37 +0800

    9/23/2022

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-
+const session = require('express-session');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-//Parsing middleware
-//Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false}));
-
 //Parse application/json
 app.use(bodyParser.json());
+//Parsing middleware
+//Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true}));
 
 //Static Files
 app.use(express.static('public'));
@@ -38,9 +38,73 @@ pool.getConnection((err, connection)=> {
 });
 
 //Routes
-const routes = require('./server/routes/employee');
-app.use('/', routes);
+const employeeroutes = require('./server/routes/employee');
+app.use('/', employeeroutes);
 const adminroutes = require('./server/routes/admin');
 app.use('/', adminroutes);
+
+const connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : 'password',
+	database : 'em_db'
+});
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
+
+// http://localhost:3000/
+app.get('/', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname + '/views/login.html'));
+});
+
+// http://localhost:3000/auth
+app.post('/auth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM employees WHERE employeeUsername = ? AND employeePassword = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/employee/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+// http://localhost:3000/home
+app.get('/employee/home', function(request, response) {
+	// If the user is loggedin
+	if (request.session.loggedin) {
+		// Output username
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		// Not logged in
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- server/routes/employee.js
+++ server/routes/employee.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const employeeController = require('../controllers/employeeController');
 
 //main employee page
-router.get('/', employeeController.mainHome)
+router.get('/employee/home', employeeController.mainHome);
 
 //view and find employee
 router.get('/employeelist', employeeController.view);
